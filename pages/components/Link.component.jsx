051397a8f56d98ac7d42529fed736f50ccce7daa
william-nod <williamkurniawan1144@gmail.com>
2022-02-15 20:30:54 +0700

    Init App

---
+++ pages/components/Link.component.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Link() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
---
+++ pages/components/LinksList.component.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Link from './Link.component'
+
+export default function LinkList() {
+  return (
+    <div>
+      <Link/>
+      <Link/>
+      <Link/>
+      <Link/>
+      <Link/>
+      <Link/>
+    </div>
+  )
+}
